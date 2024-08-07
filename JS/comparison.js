4a9b64060017c26910b7feb76535846c2d5e55bf
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2024-08-07 06:40:20 +0530

    Type Coercion in comparison.js

--- JS/comparison.js
+++ JS/comparison.js
@@ -1,3 +1,5 @@
+//Todo:- Study Type Coercion.
+
 //h Comparison Operator(<,>,<=,>=) and Equality Checks(==,===) works differently in JS.
 console.log(null > 0); //! here null is treated as 0
 console.log(null < 0);
@@ -9,3 +11,7 @@ console.log(null == 0); //!false, because equality checks will not convert null 
 
 console.log("2" == 2);
 //! here JS converts "2" to a number because the other operand is also a number.hence 2 == 2, it shows true.
+console.log("2" === 2);
+//! here JS will check the data type as well as the value.Hence string != number it will show false.
+
+//* Note: All these types of Conversions should be avoided while writing the code, but these are helpful for the interview.
