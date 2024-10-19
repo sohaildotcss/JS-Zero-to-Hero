811e391b4780be09772eedeec8db848b1d96435e
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2024-10-19 19:49:28 +0530

    Refactor code to add example of mutable arrays in JavaScript

--- JS/10_functions.js
+++ JS/10_functions.js
@@ -49,6 +49,7 @@
 //`
 
 // Callback Functions:- A callback function is a function that is passed as an argument to another function. This function is executed inside the other function. Callback functions are used in asynchronous programming. They are used to handle tasks that take some time to complete, such as reading a file or making a network request.
+
 //` Practise
 // Qs. For a given array of numbers, print the square of each value using the forEach loop.
 // const numbers = [1, 2, 3, 4, 5];
@@ -59,11 +60,12 @@
 
 // console.log(res);
 
-const arr = [5, 6, 2, 1, 101, 3];
-const output = arr.reduce((res, currVal) => {
-	return res > currVal ? res : currVal;
-});
-console.log(output);
+// Qs. Find the maximum value in an array using the reduce method.
+// const arr = [5, 6, 2, 1, 101, 3];
+// const output = arr.reduce((initialVal, currVal) => {
+// 	return initialVal > currVal ? initialVal : currVal;
+// });
+// console.log(output);
 
 //We are given array of marks of students. Filter out the marks of students that scored 90+.
 // const marks = [87, 93, 64, 99, 86];
@@ -73,6 +75,24 @@ console.log(output);
 // });
 // console.log(studentMarks);
 
-//Qs. Take a number n as input from user. Create an array of numbers from 1 to n. Use the reduce method to calculate sum of all numbers in the array. Use the reduce method to calculate product of all numbers in the array.
+//Qs. Take a number n as input from user.
+//Create an array of numbers from 1 to n.
+//Use the reduce method to calculate sum of all numbers in the array.
+//Use the reduce method to calculate product of all numbers in the array.
+// const userInput = prompt("Enter a number.");
+// const arr = [];
+// for (let i = 1; i <= userInput; i++) {
+// 	arr.push(i);
+// }
+// const sum = arr.reduce((i, k) => {
+// 	return i + k;
+// });
+
+// const multiply = arr.reduce((i, k) => {
+// 	return i * k;
+// });
+// console.log(`Array of numbers: [${arr}]`);
+// console.log(sum);
+// console.log(multiply);
 
 //`
