28e6ddbe362704a53d7abc7a1d3ad4cd0a1824a2
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2024-11-20 21:53:14 +0530

    Remove VSCode settings, update CSS with new classes, enhance HTML structure, and refine JavaScript functions for better clarity and functionality

--- JS/10_functions.js
+++ JS/10_functions.js
@@ -1,98 +1,98 @@
-// const sumFunc = (a, b) => {
-// 	return a + b;
-// };
-// console.log(sumFunc(2, 4));
+const sumFunc = (a, b) => {
+	return a + b;
+};
+console.log(sumFunc(2, 4));
 
-// const mulFunc = (a, b) => {
-// 	return a * b;
-// };
-// console.log(mulFunc(2, 4));
+const mulFunc = (a, b) => {
+	return a * b;
+};
+console.log(mulFunc(2, 4));
 
 //` Practise
 // Qs. Create a function using the "function" keyword that takes a String as an argument & returns the number of vowels in the string.
 
-// function vowelFunc(str) {
-// 	let count = 0;
-// 	for (elements of str) {
-// 		if (
-// 			elements == "a" ||
-// 			elements == "e" ||
-// 			elements == "i" ||
-// 			elements == "o" ||
-// 			elements == "u"
-// 		) {
-// 			count++;
-// 		}
-// 	}
-// 	return count;
-// }
+function vowelFunc(str) {
+	let count = 0;
+	for (elements of str) {
+		if (
+			elements == "a" ||
+			elements == "e" ||
+			elements == "i" ||
+			elements == "o" ||
+			elements == "u"
+		) {
+			count++;
+		}
+	}
+	return count;
+}
 
-// const res = vowelFunc("hello");
-// console.log(res);
+const res = vowelFunc("hello");
+console.log(res);
 
-// const countVowels = (string) => {
-// 	let count = 0;
-// 	for (let i = 0; i < string.length; i++) {
-// 		if (
-// 			string[i] == "a" ||
-// 			string[i] == "e" ||
-// 			string[i] == "i" ||
-// 			string[i] == "o" ||
-// 			string[i] == "u"
-// 		) {
-// 			count++;
-// 		}
-// 	}
-// 	return count;
-// };
-// console.log(countVowels("Sam"));
+const countVowels = (string) => {
+	let count = 0;
+	for (let i = 0; i < string.length; i++) {
+		if (
+			string[i] == "a" ||
+			string[i] == "e" ||
+			string[i] == "i" ||
+			string[i] == "o" ||
+			string[i] == "u"
+		) {
+			count++;
+		}
+	}
+	return count;
+};
+console.log(countVowels("Sam"));
 //`
 
 // Callback Functions:- A callback function is a function that is passed as an argument to another function. This function is executed inside the other function. Callback functions are used in asynchronous programming. They are used to handle tasks that take some time to complete, such as reading a file or making a network request.
 
 //` Practise
 // Qs. For a given array of numbers, print the square of each value using the forEach loop.
-// const numbers = [1, 2, 3, 4, 5];
-// const res = [];
-// numbers.forEach((num) => {
-// 	res.push(num ** 2);
-// });
+const numbers = [1, 2, 3, 4, 5];
+const res = [];
+numbers.forEach((num) => {
+	res.push(num ** 2);
+});
 
-// console.log(res);
+console.log(res);
 
 // Qs. Find the maximum value in an array using the reduce method.
-// const arr = [5, 6, 2, 1, 101, 3];
-// const output = arr.reduce((initialVal, currVal) => {
-// 	return initialVal > currVal ? initialVal : currVal;
-// });
-// console.log(output);
+const arr = [5, 6, 2, 1, 101, 3];
+const output = arr.reduce((initialVal, currVal) => {
+	return initialVal > currVal ? initialVal : currVal;
+});
+console.log(output);
 
 //We are given array of marks of students. Filter out the marks of students that scored 90+.
-// const marks = [87, 93, 64, 99, 86];
-// const studentMarks = [];
-// marks.filter((arr) => {
-// 	arr > 90 ? studentMarks.push(arr) : 0;
-// });
-// console.log(studentMarks);
+const marks = [87, 93, 64, 99, 86];
+const studentMarks = [];
+marks.filter((arr) => {
+	arr > 90 ? studentMarks.push(arr) : 0;
+});
+console.log(studentMarks);
 
 //Qs. Take a number n as input from user.
 //Create an array of numbers from 1 to n.
 //Use the reduce method to calculate sum of all numbers in the array.
 //Use the reduce method to calculate product of all numbers in the array.
-// const userInput = prompt("Enter a number.");
-// const arr = [];
-// for (let i = 1; i <= userInput; i++) {
-// 	arr.push(i);
-// }
-// const sum = arr.reduce((i, k) => {
-// 	return i + k;
-// });
+const userInput = prompt("Enter a number.");
+const arr = [];
+for (let i = 1; i <= userInput; i++) {
+	arr.push(i);
+}
+const sum = arr.reduce((i, k) => {
+	return i + k;
+});
 
-// const multiply = arr.reduce((i, k) => {
-// 	return i * k;
-// });
-// console.log(`Array of numbers: [${arr}]`);
-// console.log(sum);
-// console.log(multiply);
+const multiply = arr.reduce((i, k) => {
+	return i * k;
+});
+console.log(`Array of numbers: [${arr}]`);
+console.log(sum);
+console.log(multiply);
 
 //`
--- JS/DOM/script.js
+++ JS/DOM/script.js
@@ -1,5 +1,5 @@
 const id = document.getElementById("parfa-tag");
-// console.log(id);
+console.log(id);
 //! Invalid id will return null
 
 const my_class = document.getElementsByClassName("headding");
@@ -12,11 +12,11 @@ const tagName = document.getElementsByTagName("p");
 
 const query = document.querySelector(".my_class");
 // console.dir(query);
-//! Display the first element with the class heading
+//! Display the first element with the class .my_class
 
 const queryAll = document.querySelectorAll(".my_class");
 // console.dir(queryAll);
-//! Display all the element with the class my_class
+//! Display all the element with the class .my_class
 
 //*Properties //*
 
@@ -31,3 +31,57 @@ console.log(firstChild);
 //lastChild:- It will return the last child of the element and if there is a whitespace between the element then it will return the whitespace as well as text node as shown below.
 const lastChild = document.querySelector(".div-tag").lastChild;
 console.dir(lastChild);
+
+console.log(document.body.innerText);
+console.log(document.body.innerHTML);
+console.log(document.body.textContent);
+//!textContent can return the text that is hidden by CSS but innerText can't.
+
+//` Practice
+// Qs. Create a H2 heading element with text - "Hello JavaScript". Append "from Apna College students" to this text using JS.
+let myClass = document.querySelector("h2");
+let newClass = " from Apna College";
+myClass.innerText = myClass.innerText + newClass;
+
+// Qs. Create 3 divs with common class name - "box". Access them & add some unique text to each of them.
+// let access = document.querySelectorAll(".box");
+
+// access[0].innerText = "TESTxxds";
+// access[1].innerText = "dsdsd";
+// access[2].innerText = "ij[jw";
+
+let test = document.querySelector(".box");
+test.firstChild.textContent = "name";
+// test.secondChild.textContent = "age";
+
+//`
+
+// node.append( el ) adds at the end of node (inside)
+let divTest = document.querySelector("#test");
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click Me";
+divTest.append(newBtn);
+
+// node.prepend( eI ) adds at the start of node (inside)
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hello</i>";
+document.querySelector("body").prepend(newHeading);
+
+// node.before( eI ) adds before the node (outside)
+// node.after( eI ) adds after the node (outside)
+
+//` Practise
+//Q. Create a new button element. Give it a text "click me", background color of red & text color of white. Insert the button as the first element inside the body tag.
+let btn = document.createElement("button");
+btn.innerText = "click me";
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+document.querySelector("body").prepend(btn);
+
+//Q.Create a p tag in html, give it a class & some styling. Now create a new class in CSS and try to append this class to the element.
+let practise_p = document.querySelector("#p");
+// practise_p.setAttribute("class", "newClass"); //!the setAttribute method will overwrite the existing class name.
+
+//Did you notice, how you overwrite the class name when you add a new one ? Solve this problem using classList.
+practise_p.classList.add("newClass"); //!but the classList method will not overwrite the existing class name, instead it will add the new class keeping the existing class name as it is.
+//`
