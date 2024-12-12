72716d7953116b9651bd8f94c575eeeaa2ff7022
sohaildotcss <sohailqwerty7@gmail.com>
2024-12-12 19:39:54 +0530

    Refactor Tic Tac Toe game logic; add reset button and enhance UI styling

--- JS/tic-tac-toe/app.js
+++ JS/tic-tac-toe/app.js
@@ -11,39 +11,32 @@ let winPatterns = [
 	[2, 4, 6], //diagonal
 ];
 
-const test = (event) => {
-	const box = event.target;
-	if (initialPlayer === "cross") {
-		box.innerHTML = "X";
-		initialPlayer = "circle";
-		box.disabled = "true";
-		box.style.color = "#758bfd";
-	} else {
-		box.innerHTML = "O";
-		initialPlayer = "cross";
-		box.disabled = "true";
-		box.style.color = "#e09f3e";
-	}
-	checkWinner();
-};
-
 let initialPlayer = "cross";
 boxes.forEach((box) => {
-	box.addEventListener("click", test);
+	box.addEventListener("click", () => {
+		if (initialPlayer === "cross") {
+			box.innerHTML = "X";
+			initialPlayer = "circle";
+			box.disabled = "true";
+			box.style.color = "#758bfd";
+		} else {
+			box.innerHTML = "O";
+			initialPlayer = "cross";
+			box.disabled = "true";
+			box.style.color = "#e09f3e";
+		}
+		checkWinner();
+	});
 });
 
 const checkWinner = () => {
 	for (pattern of winPatterns) {
-		let pos1 = boxes[pattern[0]];
-		let pos2 = boxes[pattern[1]];
-		let pos3 = boxes[pattern[2]];
-		if (pos1.innerText != "" && pos2.innerText != "" && pos3.innerText != "") {
-			if (
-				pos1.innerText == pos2.innerText &&
-				pos2.innerText == pos3.innerText
-			) {
-				console.log(pos1.value);
-				winnerText.innerText = `The winner is ${pos1.value}`;
+		let pos1 = boxes[pattern[0]].innerText;
+		let pos2 = boxes[pattern[1]].innerText;
+		let pos3 = boxes[pattern[2]].innerText;
+		if (pos1 != "" && pos2 != "" && pos3 != "") {
+			if (pos1 == pos2 && pos2 == pos3) {
+				winnerText.innerText = `The Winner is "${pos1}"`;
 				disableButtons();
 			}
 		}
@@ -52,11 +45,16 @@ const checkWinner = () => {
 
 //disable all buttons after the winner is declared
 const disableButtons = () => {
-	boxes.forEach((box) => {
-		box.removeEventListener("click", test);
+	for (box of boxes) {
+		box.disabled = true;
 		box.style.backgroundColor = "#f1faee";
 		box.style.border = "none";
-		// box.style.boxShadow = "none";
 		box.style.cursor = "auto";
-	});
+	}
 };
+
+//reset game
+let resetBtn = document.querySelector("#reset-button");
+resetBtn.addEventListener("click", () => {
+	location.reload();
+});
