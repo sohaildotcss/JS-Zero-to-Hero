36ee22376d3d60ebfee3c7e61fe8280165a3d45b
sohaildotcss <sohailqwerty7@gmail.com>
2025-01-13 23:35:33 +0530

    Add error handling demonstration in 12_error-handling.js

---
+++ JS/12_error-handling.js
@@ -0,0 +1,18 @@
+//Err
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+try {
+	console.log(a + c);
+} catch (err) {
+	console.log("Error is ", err);
+}
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
