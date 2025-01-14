c64c9b093a38446bea050f10c721414f502c7288
sohaildotcss <sohailqwerty7@gmail.com>
2025-01-14 21:55:13 +0530

    Wrap up for today!

--- JS/14_promises.js
+++ JS/14_promises.js
@@ -41,7 +41,7 @@
 //To solve this callback hell, Promise are used
 
 //* Promise
-// Promises have 3 states:-
+// Promise is an object and have 3 states:-
 // Pending: the result is undefined
 // Resolve: the result is a value (fulfilled)
 // Reject: the result is an error object.
@@ -65,5 +65,37 @@ function fun(data) {
 }
 let sam = fun(1); //promise returned by func is stored in a variable
 
+
 //! In general we don't create any promises, Promises are returned by api's and
-//! we store them in a variable and perform required operations. (as shown in above)
+//! we just store them in a variable and perform required operations. (as shown in above)
+
+//How to use promises?
+// Whenever we recieve a promise it comes with either resolve() or reject(),
+// for example if we make an api call either we recieve the data or recieve the error.
+
+//* .then() & .catch()
+// .then is used if the promise is fulfilled and .catch is used if the promise rejects.
+// The function passed in .then() is executed only if the promise is fulfilled.
+
+const apiCall = () => {
+	return new Promise((resolve, reject) => {
+		console.log("data recieved");
+		resolve("success"); //we can pass any msg in the resolve
+		reject("error"); //we can pass any msg in the reject
+	});
+};
+let storedData = apiCall(); //promise is being stored in a variable
+
+// If the above promise is fulfilled then automatically the below code block will be executed .
+storedData.then((res) => {
+	console.log(res, "Promise resolved"); //.then() comes with default parameter, "res"(we can name it anything) in which the already passed down resolve message is stored. ref:line.82
+});
+
+// Similiarly, if the above promise(line.79) is rejected then automatically the below code block will be executed.
+storedData.catch((err) => {
+	console.log(err, "Error occurred"); //.catch() also comes with default parameter, "err"(we can name it anything) in which the already passed down reject message is stored. ref:line.83
+});
+
+//! From resolve() and reject() only one will be executed everytime, not both.
+
+//* Promise Chaining:-
